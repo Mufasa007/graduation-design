d21e5683e1f377a0e9ebb53ffe1c12b76b474def
Wan Yu <594564264@qq.com>
2016-04-01 18:13:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 版本1.0的初步设计
+# graduation-design
---
+++ app/src/main/java/com/mufasa007/link_nunber_011/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mufasa007.link_nunber_011;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lcontacts);
+        //其实本质上，这就是java程序
+        /*这也是我之前一上来就学android开发失败的原因*/
+    }
+}
